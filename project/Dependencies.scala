6f6fe8ecb4d78992c958853358420a5fd691f2e7
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-09-27 22:51:58 +0530

    upgrad tmt-typed dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,9 +22,9 @@ object Dependencies {
   val `scalatest`      = dep("org.scalatest" %%% "scalatest" % "3.0.8")
   val `scalajs-dom`    = dep("org.scala-js" %%% "scalajs-dom" % "0.9.7")
 
-  val `std`                      = dep("com.github.tmtsoftware.tmt-typed" %%% "std"                      % "8b87baf")
-  val `eventsource`              = dep("com.github.tmtsoftware.tmt-typed" %%% "eventsource"              % "8b87baf")
-  val `rsocket-websocket-client` = dep("com.github.tmtsoftware.tmt-typed" %%% "rsocket-websocket-client" % "8b87baf")
+  val `std`                      = dep("com.github.tmtsoftware.tmt-typed" %%% "std"                      % "315a809")
+  val `eventsource`              = dep("com.github.tmtsoftware.tmt-typed" %%% "eventsource"              % "315a809")
+  val `rsocket-websocket-client` = dep("com.github.tmtsoftware.tmt-typed" %%% "rsocket-websocket-client" % "315a809")
 
   val `rsocket-transport-akka` = "com.github.mushtaq"         % "rsocket-transport-akka" % "740a0a7"
   val `rsocket-core`           = "io.rsocket"                 % "rsocket-core"           % "0.11.18"

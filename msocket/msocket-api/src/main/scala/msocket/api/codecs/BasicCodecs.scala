ea8c66cca2edf2569d317badf98f7b06695edb3d
Mushtaq Ahmed <mushtaq.a@gmail.com>
2019-11-20 09:17:47 +0530

    add timeout codec

--- msocket/msocket-api/src/main/scala/msocket/api/codecs/BasicCodecs.scala
+++ msocket/msocket-api/src/main/scala/msocket/api/codecs/BasicCodecs.scala
@@ -1,8 +1,14 @@
 package msocket.api.codecs
 
+import java.util.concurrent.TimeUnit
+
 import akka.Done
+import akka.util.Timeout
+import io.bullet.borer.derivation.ArrayBasedCodecs
 import io.bullet.borer.{Codec, Decoder, Encoder}
 import msocket.api.models.Result
+
+import scala.concurrent.duration.FiniteDuration
 
 trait BasicCodecs {
   implicit def eitherEnc[E: Encoder, S: Encoder]: Encoder[Either[E, S]] =
@@ -12,4 +18,12 @@ trait BasicCodecs {
     implicitly[Decoder[Result[S, E]]].map(_.toEither)
 
   implicit lazy val doneCodec: Codec[Done] = Codec.bimap[String, Done](_ => "done", _ => Done)
+
+  implicit lazy val durationCodec: Codec[FiniteDuration] = Codec.bimap[(Long, String), FiniteDuration](
+    finiteDuration => (finiteDuration.length, finiteDuration.unit.toString),
+    { case (length, unitStr) => FiniteDuration(length, TimeUnit.valueOf(unitStr)) }
+  )
+
+  implicit lazy val timeoutCodec: Codec[Timeout] = ArrayBasedCodecs.deriveUnaryCodec
+
 }

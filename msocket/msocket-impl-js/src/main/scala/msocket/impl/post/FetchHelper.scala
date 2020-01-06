85dac0d6c89e8502903a9f4423303db6b64559dd
Mushtaq Ahmed <mushtaq.a@gmail.com>
2020-01-06 17:54:21 +0530

    fix error handling in http-post-js

--- msocket/msocket-impl-js/src/main/scala/msocket/impl/post/FetchHelper.scala
+++ msocket/msocket-impl-js/src/main/scala/msocket/impl/post/FetchHelper.scala
@@ -2,6 +2,7 @@ package msocket.impl.post
 
 import io.bullet.borer.Encoder
 import msocket.api.Encoding.JsonText
+import msocket.api.ErrorProtocol
 import msocket.api.models.{HttpError, ServiceError}
 import org.scalajs.dom.experimental.{Fetch, HttpMethod, Response}
 
@@ -10,22 +11,30 @@ import scala.scalajs.js
 import scala.util.control.NonFatal
 
 object FetchHelper {
-  def postRequest[Req: Encoder](uri: String, req: Req)(implicit ec: ExecutionContext): Future[Response] = {
+  def postRequest[Req: Encoder](uri: String, req: Req)(implicit ec: ExecutionContext, ep: ErrorProtocol[Req]): Future[Response] = {
     val fetchRequest = new FetchRequest {
       method = HttpMethod.POST
       body = JsonText.encode(req)
       headers = js.Dictionary("content-type" -> "application/json")
     }
+
+    def handleError(response: Response): Future[Throwable] = {
+      response.text().toFuture.map { msg =>
+        try JsonText.decode[ep.E](msg)
+        catch {
+          case NonFatal(ex) =>
+            try JsonText.decode[ServiceError](msg)
+            catch {
+              case NonFatal(ex) => HttpError(response.status, response.statusText, msg)
+            }
+        }
+      }
+    }
+
     Fetch.fetch(uri, fetchRequest).toFuture.flatMap { response =>
       response.status match {
         case 200 => Future.successful(response)
-        case statusCode =>
-          response.text().toFuture.map { msg =>
-            try throw JsonText.decode[ServiceError](msg)
-            catch {
-              case NonFatal(ex) => throw HttpError(statusCode, response.statusText, msg)
-            }
-          }
+        case _   => handleError(response).map(throw _)
       }
     }
   }

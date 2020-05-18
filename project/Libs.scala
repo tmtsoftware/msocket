0d7501e680d6a9a6ddd4827c8a26a39329b5c7c2
mushtaq <mushtaq.a@gmail.com>
2020-05-18 20:25:29 +0530

    upgrade versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -3,24 +3,23 @@ import sbt.Def.{setting => dep}
 import sbt._
 
 object Libs {
-  val `akka-stream`         = "com.typesafe.akka" %% "akka-stream"         % "2.6.4"
-  val `akka-actor-typed`    = "com.typesafe.akka" %% "akka-actor-typed"    % "2.6.4"
-  val `akka-stream-testkit` = "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.4"
+  val `akka-stream`         = "com.typesafe.akka" %% "akka-stream"         % "2.6.5"
+  val `akka-actor-typed`    = "com.typesafe.akka" %% "akka-actor-typed"    % "2.6.5"
+  val `akka-stream-testkit` = "com.typesafe.akka" %% "akka-stream-testkit" % "2.6.5"
 
   val `akka-http`         = "com.typesafe.akka" %% "akka-http"         % "10.2.0-M1"
   val `akka-http-testkit` = "com.typesafe.akka" %% "akka-http-testkit" % "10.2.0-M1"
 
-  val `borer-core`        = dep("io.bullet" %%% "borer-core" % "1.5.0")
-  val `borer-derivation`  = dep("io.bullet" %%% "borer-derivation" % "1.5.0")
-  val `borer-compat-akka` = "io.bullet" %% "borer-compat-akka" % "1.5.0"
+  val `borer-core`        = dep("io.bullet" %%% "borer-core" % "1.6.0")
+  val `borer-derivation`  = dep("io.bullet" %%% "borer-derivation" % "1.6.0")
+  val `borer-compat-akka` = "io.bullet" %% "borer-compat-akka" % "1.6.0"
 
-  val `akka-http-cors` = "ch.megard" %% "akka-http-cors" % "0.4.2"
-  val `scalatest`      = dep("org.scalatest" %%% "scalatest" % "3.1.1")
+  val `akka-http-cors` = "ch.megard" %% "akka-http-cors" % "0.4.3"
+  val `scalatest`      = dep("org.scalatest" %%% "scalatest" % "3.1.2")
   val `scalajs-dom`    = dep("org.scala-js" %%% "scalajs-dom" % "1.0.0")
 
-  val `std`                      = dep("org.scalablytyped" %%% "std"                      % "3.8-b0a61d")
-  val `eventsource`              = dep("org.scalablytyped" %%% "eventsource"              % "1.1.2-2de663")
-  val `rsocket-websocket-client` = dep("org.scalablytyped" %%% "rsocket-websocket-client" % "0.0.3-848184")
+  val `eventsource`              = dep("org.scalablytyped" %%% "eventsource" % "1.1.2-3917e7")
+  val `rsocket-websocket-client` = dep("org.scalablytyped" %%% "rsocket-websocket-client" % "0.0.3-6ed313")
 
   val `rsocket-core`            = "io.rsocket" % "rsocket-core"            % "1.0.0-RC5"
   val `rsocket-transport-netty` = "io.rsocket" % "rsocket-transport-netty" % "1.0.0-RC5"
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,8 @@
-addSbtPlugin("com.timushev.sbt"   % "sbt-updates"              % "0.5.0")
-addSbtPlugin("org.scalameta"      % "sbt-scalafmt"             % "2.3.2")
+addSbtPlugin("com.timushev.sbt"   % "sbt-updates"              % "0.5.1")
+addSbtPlugin("org.scalameta"      % "sbt-scalafmt"             % "2.4.0")
 addSbtPlugin("org.portable-scala" % "sbt-scalajs-crossproject" % "1.0.0")
-addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % "1.0.1")
-addSbtPlugin("ch.epfl.scala"      % "sbt-scalajs-bundler"      % "0.17.0")
+addSbtPlugin("org.scala-js"       % "sbt-scalajs"              % "1.1.0")
+addSbtPlugin("ch.epfl.scala"      % "sbt-scalajs-bundler"      % "0.18.0")
 addSbtPlugin("io.spray"           % "sbt-revolver"             % "0.9.1")
 
 libraryDependencies += "com.sun.activation" % "javax.activation" % "1.2.0"

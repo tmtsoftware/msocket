ee8bbee92319acfc7474d96759116c9de8d3aa2b
mushtaq <mushtaq.a@gmail.com>
2020-08-09 11:52:43 +0530

    use type actor system for Http

--- example-service/example-service-server/src/main/scala/msocket/example/server/ExampleServer.scala
+++ example-service/example-service-server/src/main/scala/msocket/example/server/ExampleServer.scala
@@ -1,19 +1,16 @@
 package msocket.example.server
 
-import akka.actor
 import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-class ExampleServer(routes: Route)(system: ActorSystem[_]) {
+class ExampleServer(routes: Route)(implicit system: ActorSystem[_]) {
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
 
   def start(host: String, port: Int): Future[Http.ServerBinding] = {
-    implicit val classic: actor.ActorSystem = system.toClassic
     Http().newServerAt(host, port).bind(routesWithCors)
   }
 

70d186270dc2c821b50e0f4f7eaccd42736e9f0e
poorva <poorvasgokhale@gmail.com>
2020-09-17 16:21:21 +0530

    #ESW-382: Mausam | Poorva: Update version
     Co-authored-by:  Mausam Yede <mausam.yede1@gmail.com>

--- project/Libs.scala
+++ project/Libs.scala
@@ -6,10 +6,10 @@ object Libs {
   // 1.0.0-M1 does not work with Scala.js js yet
   val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "0.10.0"
 
-  val `akka-stream`              = "com.typesafe.akka" %% "akka-stream"              % "2.6.8"
-  val `akka-actor-typed`         = "com.typesafe.akka" %% "akka-actor-typed"         % "2.6.8"
-  val `akka-actor-testkit-typed` = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.8"
-  val `akka-stream-testkit`      = "com.typesafe.akka" %% "akka-stream-testkit"      % "2.6.8"
+  val `akka-stream`              = "com.typesafe.akka" %% "akka-stream"              % "2.6.9"
+  val `akka-actor-typed`         = "com.typesafe.akka" %% "akka-actor-typed"         % "2.6.9"
+  val `akka-actor-testkit-typed` = "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.9"
+  val `akka-stream-testkit`      = "com.typesafe.akka" %% "akka-stream-testkit"      % "2.6.9"
 
   val `akka-http`         = "com.typesafe.akka" %% "akka-http"         % "10.2.0"
   val `akka-http-testkit` = "com.typesafe.akka" %% "akka-http-testkit" % "10.2.0"
@@ -26,8 +26,8 @@ object Libs {
   val eventsource                = dep("org.scalablytyped" %%% "eventsource" % "1.1.4-5269e8")
   val `rsocket-websocket-client` = dep("org.scalablytyped" %%% "rsocket-websocket-client" % "0.0.3-280a1e")
 
-  val `rsocket-core`            = "io.rsocket" % "rsocket-core"            % "1.0.1"
-  val `rsocket-transport-netty` = "io.rsocket" % "rsocket-transport-netty" % "1.0.1"
+  val `rsocket-core`            = "io.rsocket" % "rsocket-core"            % "1.0.2"
+  val `rsocket-transport-netty` = "io.rsocket" % "rsocket-transport-netty" % "1.0.2"
 }
 
 object Prometheus {

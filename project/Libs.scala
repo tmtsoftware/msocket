c47b645dc009084a9c966e0edfe08344afba7f16
Mushtaq Ahmed <mushtaq.a@gmail.com>
2021-10-17 22:49:31 +0530

    few more version upgrades

--- project/Libs.scala
+++ project/Libs.scala
@@ -5,7 +5,7 @@ import sbt._
 object Libs {
   val `scala-async` = "org.scala-lang.modules" %% "scala-async" % "1.0.1"
 
-  private val akkaVersion = "2.6.16"
+  private val akkaVersion = "2.6.17"
 
   val `akka-stream`              = "com.typesafe.akka" %% "akka-stream"              % akkaVersion
   val `akka-actor-typed`         = "com.typesafe.akka" %% "akka-actor-typed"         % akkaVersion
@@ -23,9 +23,9 @@ object Libs {
   val `borer-compat-akka`  = "io.bullet" %% "borer-compat-akka" % borerVersion
 
   val `akka-http-cors` = "ch.megard"         %% "akka-http-cors" % "1.1.2"
-  val scalatest        = dep("org.scalatest" %%% "scalatest" % "3.2.9")
+  val scalatest        = dep("org.scalatest" %%% "scalatest" % "3.2.10")
   val `selenium-3-141` = "org.scalatestplus" %% "selenium-3-141" % "3.2.9.0"
-  val `scalajs-dom`    = dep("org.scala-js" %%% "scalajs-dom" % "1.1.0")
+  val `scalajs-dom`    = dep("org.scala-js" %%% "scalajs-dom" % "1.2.0")
 
   val `tmt-typed` = dep("com.github.mushtaq.tmt-typed" %%% "tmt-typed" % "8dc3e04")
 

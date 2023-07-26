e0b36438551e530c37156318a60c928b313bf41b
Allan Brighton <Allan.Brighton@t-online.de>
2023-07-26 19:54:03 +0200

    Updated pekko and borer versions

--- project/Libs.scala
+++ project/Libs.scala
@@ -6,23 +6,22 @@ object Libs {
   val `scala-async` = dep("org.scala-lang.modules" %%% "scala-async" % "1.0.1")
   val `scala-java8-compat` = "org.scala-lang.modules" %% "scala-java8-compat" % "1.0.2" // BSD 3-clause "New" or "Revised" License
 
-  private val pekkoVersion = "1.0.0"
-  val `pekko-stream`              = "org.apache.pekko" %% "pekko-stream"              % pekkoVersion
-  val `pekko-actor-typed`         = "org.apache.pekko" %% "pekko-actor-typed"         % pekkoVersion
-  val `pekko-actor-testkit-typed` = "org.apache.pekko" %% "pekko-actor-testkit-typed" % pekkoVersion
-  val `pekko-stream-testkit`      = "org.apache.pekko" %% "pekko-stream-testkit"      % pekkoVersion
+  private val pekkoVersion = "1.0.1"
+    val pekkoOrg = "org.apache.pekko"
+//  val pekkoOrg = "com.github.apache.incubator-pekko"
+  val `pekko-stream`              = pekkoOrg %% "pekko-stream"              % pekkoVersion
+  val `pekko-actor-typed`         = pekkoOrg %% "pekko-actor-typed"         % pekkoVersion
+  val `pekko-actor-testkit-typed` = pekkoOrg %% "pekko-actor-testkit-typed" % pekkoVersion
+  val `pekko-stream-testkit`      = pekkoOrg %% "pekko-stream-testkit"      % pekkoVersion
 
-  private val pekkoHttpVersion = "1.0.0-RC1"
+  private val pekkoHttpVersion = "1.0.0-RC2"
 //  val pekkoHttpOrg = "org.apache.pekko"
   val pekkoHttpOrg = "com.github.apache.incubator-pekko-http"
-//  val `pekko-http` = pekkoHttpOrg %% "pekko-http" % pekkoHttpVersion
-//  val `pekko-http-cors` = pekkoHttpOrg %% "pekko-http-cors" % pekkoHttpVersion
-//  val `pekko-http-testkit` = pekkoHttpOrg %% "pekko-http-testkit" % pekkoHttpVersion
   val `pekko-http` = pekkoHttpOrg %% "pekko-http" % pekkoHttpVersion
   val `pekko-http-cors` = pekkoHttpOrg %% "pekko-http-cors" % pekkoHttpVersion
   val `pekko-http-testkit` = pekkoHttpOrg %% "pekko-http-testkit" % pekkoHttpVersion
 
-  private val borerVersion = "5875c8a597a82fabf740975a4f4c7d70c1eb5114"
+  private val borerVersion = "d59e5fd"
   //  val borerOrg     = "io.bullet"
   val borerOrg = "com.github.tmtsoftware.borer"
   val `borer-core` = dep(borerOrg %%% "borer-core" % borerVersion)

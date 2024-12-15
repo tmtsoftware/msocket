b55e81bae0f018a99de55e0bbe7f87c2b4ee4ab1
Allan Brighton <Allan.Brighton@t-online.de>
2024-12-15 23:08:51 +0100

    Dependency updates

--- project/Libs.scala
+++ project/Libs.scala
@@ -4,30 +4,30 @@ import sbt._
 
 object Libs {
   val `scala-js-macrotask-executor` = dep("org.scala-js" %%% "scala-js-macrotask-executor" % "1.1.1")
-  val `dotty-cps-async` = dep("com.github.rssh" %%% "dotty-cps-async" % "0.9.21")
+  val `dotty-cps-async` = dep("com.github.rssh" %%% "dotty-cps-async" % "0.9.23")
   val `shim-scala-async-dotty-cps-async` = "com.github.rssh" %% "shim-scala-async-dotty-cps-async" % "0.9.17"
 
-  private val pekkoVersion = "1.0.2"
+  private val pekkoVersion = "1.1.2"
     val pekkoOrg = "org.apache.pekko"
   val `pekko-stream`              = pekkoOrg %% "pekko-stream"              % pekkoVersion
   val `pekko-actor-typed`         = pekkoOrg %% "pekko-actor-typed"         % pekkoVersion
   val `pekko-actor-testkit-typed` = pekkoOrg %% "pekko-actor-testkit-typed" % pekkoVersion
   val `pekko-stream-testkit`      = pekkoOrg %% "pekko-stream-testkit"      % pekkoVersion
 
-  private val pekkoHttpVersion = "1.0.1"
+  private val pekkoHttpVersion = "1.1.0"
   val pekkoHttpOrg = "org.apache.pekko"
   val `pekko-http` = pekkoHttpOrg %% "pekko-http" % pekkoHttpVersion
   val `pekko-http-cors` = pekkoHttpOrg %% "pekko-http-cors" % pekkoHttpVersion
   val `pekko-http-testkit` = pekkoHttpOrg %% "pekko-http-testkit" % pekkoHttpVersion
 
-  private val borerVersion = "1.14.0"
+  private val borerVersion = "1.15.0"
     val borerOrg     = "io.bullet"
 //  val borerOrg = "com.github.tmtsoftware.borer"
   val `borer-core` = dep(borerOrg %%% "borer-core" % borerVersion)
   val `borer-derivation` = dep(borerOrg %%% "borer-derivation" % borerVersion)
   val `borer-compat-pekko` = borerOrg %% "borer-compat-pekko" % borerVersion
 
-  val scalatest        = dep("org.scalatest" %%% "scalatest" % "3.2.18")
+  val scalatest        = dep("org.scalatest" %%% "scalatest" % "3.2.19")
   val `selenium-3-141` = "org.scalatestplus" %% "selenium-3-141" % "3.2.10.0"
   val `scalajs-dom`    = dep("org.scala-js" %%% "scalajs-dom" % "2.8.0")
 
